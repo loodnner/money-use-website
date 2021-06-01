04d6994d6ea67cd7b0ce6a1d20902617876e3a76
loodnn <loudjane@163.com>
2021-06-01 10:53:28 +0800

    deploy

---
+++ precache-manifest.c52d07c8c1426161af86f32d468b8019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f909a0b5a263fe635b",
+    "url": "/money-use-website/css/app.5a561075.css"
+  },
+  {
+    "revision": "40550661baa1f230ed10708263befdcd",
+    "url": "/money-use-website/index.html"
+  },
+  {
+    "revision": "92f909a0b5a263fe635b",
+    "url": "/money-use-website/js/app.214ff5d5.js"
+  },
+  {
+    "revision": "ac5dc98f00501e831cb0",
+    "url": "/money-use-website/js/chunk-vendors.a7a0b7e0.js"
+  },
+  {
+    "revision": "9f69b74e51cb2dc1e0e72dbeefb5f1ce",
+    "url": "/money-use-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-use-website/robots.txt"
+  }
+]);
